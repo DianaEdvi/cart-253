356ce6efc3f9ce887ee52594060e311e2bf1a03c
Diana <edvi.diana@gmail.com>
2024-09-10 01:44:43 -0400

    Created existential crisis program

---
+++ Week_2-Instructions/js/script.js
@@ -0,0 +1,28 @@
+/**
+ * Function Calls
+ * Diana Edvi
+ * 
+ * Existential crisis program.
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    //Once at the beginning of the program
+    createCanvas(640,480);
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    //every frame
+    background(255, 100, 100);
+    rect(200, 20, 240, 320);
+
+
+}
