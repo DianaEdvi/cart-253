5241f0e7d22b2b3abdca4e358dd1fd5d9e528454
Diana <edvi.diana@gmail.com>
2024-09-10 13:21:30 -0400

    Completed challenge 3

--- Week_2_Instructions/5_Challenges/3_Chain/js/script.js
+++ Week_2_Instructions/5_Challenges/3_Chain/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640,640);
 }
 
 
@@ -20,5 +20,43 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background("yellow");
+
+    //Ellipse 1
+    push();
+    strokeWeight(20);
+    noFill();
+    ellipse(320, 0, 150, 200);
+    pop();
+
+    //Ellipse 2
+    push();
+    strokeWeight(20);
+    noFill();
+    ellipse(320, 150, 150, 200);
+    pop();
+
+    //Ellipse 3
+    push();
+    strokeWeight(20);
+    noFill();
+    ellipse(320, 300, 150, 200);
+    pop();
+
+    //Ellipse 4
+    push();
+    strokeWeight(20);
+    noFill();
+    ellipse(320, 450, 150, 200);
+    pop();
+
+    //Ellipse 5
+    push();
+    strokeWeight(20);
+    noFill();
+    ellipse(320, 600, 150, 200);
+    pop();
+
 
 }
+
