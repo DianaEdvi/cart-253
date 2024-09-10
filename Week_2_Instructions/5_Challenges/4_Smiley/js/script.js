db367f6e3b8b10b48b882681e5fea8b6c5fdfa33
Diana <edvi.diana@gmail.com>
2024-09-10 13:37:21 -0400

    Completed challenge 4

--- Week_2_Instructions/5_Challenges/4_Smiley/js/script.js
+++ Week_2_Instructions/5_Challenges/4_Smiley/js/script.js
@@ -1,18 +1,17 @@
 /**
- * Title of Project
- * Author Name
+ * Smiley
+ * Diana Edvi
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Draws smiley face
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates canvas
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
@@ -20,5 +19,34 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background("yellow");
+    drawSmiley()
+}
 
+function drawSmiley() {
+    //circle
+    push();
+    noFill();
+    strokeWeight(20);
+    ellipse(320, 320, 500);
+    pop();
+
+    //eye 1
+    push();
+    fill("black");
+    ellipse(215, 250, 75, 125);
+    pop();
+
+    //eye 2
+    push();
+    fill("black");
+    ellipse(425, 250, 75, 125);
+    pop();
+
+    //mouth
+    push();
+    noFill();
+    strokeWeight(20)
+    arc(320, 300, 375, 375, 0, PI);
+    pop();
 }
