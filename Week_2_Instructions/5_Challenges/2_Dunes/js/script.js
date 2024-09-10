e4da01529076ec61ced2d61f56bc3332188d3b16
Diana <edvi.diana@gmail.com>
2024-09-10 03:38:23 -0400

    Completed challenge 2

--- Week_2_Instructions/5_Challenges/2_Dunes/js/script.js
+++ Week_2_Instructions/5_Challenges/2_Dunes/js/script.js
@@ -1,24 +1,55 @@
 /**
- * Title of Project
- * Author Name
+ * Italian flag
+ * Diana Edvi
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Creates a drawing of a pyramid.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas for our masterpiece
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draw's yellow pyramid on a blue background.
 */
 function draw() {
+    //Draw background
+    push();
+    background(62, 144, 237);
+    pop();
+
+    //Draw sand 1
+    push();
+    noStroke();
+    fill("yellow");
+    ellipse(50, 600, 600);
+    pop();
+
+    //Draw sand 2
+    push();
+    noStroke();
+    fill("yellow");
+    ellipse(400, 650, 600, 500);
+    pop();
+
+    //Draw pyramid 1
+    push();
+    noStroke();
+    fill(224, 217, 13);
+    triangle(120, 500, 275, 250, 350, 550);
+    pop();
+
+    //Draw pyramid 2
+    push();
+    noStroke();
+    fill(163, 160, 64);
+    triangle(450, 450, 275, 250, 350, 550);
+    pop();
 
 }
