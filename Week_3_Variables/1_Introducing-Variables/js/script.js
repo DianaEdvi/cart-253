41d5011851040e6eaab3850f73004bc7c08a50b7
Diana <edvi.diana@gmail.com>
2024-09-10 15:03:13 -0400

    Added variables project

---
+++ Week_3_Variables/1_Introducing-Variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Variables
+ * Diana Edvi
+ * 
+ * comments suck
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
