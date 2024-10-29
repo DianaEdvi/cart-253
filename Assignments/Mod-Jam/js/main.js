60333b3d0d61615fe8cf889f65aed71212b817c2
Diana <edvi.diana@gmail.com>
2024-10-29 17:00:46 -0400

    Did you know there were still comments to add?

--- Assignments/Mod-Jam/js/main.js
+++ Assignments/Mod-Jam/js/main.js
@@ -1,13 +1,13 @@
 "use strict";
 
-let state = "";
-let clicked = false;
-let totalFlies = 0;
-let canvas = undefined;
-// let png = undefined;
-let spacePressed = false;
+let state = ""; //The state the game is in
+let clicked = false; //Whether the mouse has been clicked or not
+let totalFlies = 0; //The total flies the player has
+let canvas = undefined; //The canvas
 
-
+/**
+ * The main preload function that preloads all the other preload functions
+ */
 function preload() {
     preloadOptions();
     preloadDecoration();
@@ -16,56 +16,58 @@ function preload() {
 
 /**
  * Creates the canvas and initializes the fly
+ * Also calls other setup functions
  */
 function setup() {
     canvas = createCanvas(1440, 810);
     state = "title";
-    // setupDecoratingGame();
     setupFrogFrogFrog();
     setupDecoratingGame();
+    //Create the base png for the end
     UI.endUI.png.img = createImage(490, 405);
-
 }
 
+/**
+ * The main draw function. Calls all the other draw functions depending on which state the game is in
+ */
 function draw() {
     if (state === "title") {
         drawMenu();
     } else if (state === "frog") {
         drawFrogFrogFrog();
     } else if (state === "choose") {
-        drawOptions(); //sets options and draws selection menu and outlines
+        drawOptions(); //Sets options and draws selection menu and outlines
     } else if (state === "decorate") {
         drawDecoration();
-        // drawTempImg();
     } else if (state === "finished") {
         drawEnd();
-        // drawPNG();
     }
 }
 
-function menuController() {
-    //RESET EVERYTHING
-    resetGame();
-}
-
+/**
+ * The main mouseClicked function
+ * Handles mouse clicked logic depending on the state the game is in
+ */
 function mouseClicked() {
     clicked = true;
     if (state === "title") {
-        menuController();
-        // buttonHandlerMenu();
+        //Reset the game
+        resetGame();
     } else if (state === "choose") {
         outlineSelections(); //sets palette and background
-        buttonHandlerOptions(buttons.ready);
+        buttonHandlerOptions(buttons.ready); //Handles button logic for "ready" button (exception)
         // check = buttonCheck();
     } else if (state === "decorate") {
-        decoMouseClicked();
-        buttonHandlerOptions(buttons.finished);
-        // buttonHandlerDecorate();
+        decoMouseClicked(); //Handle mouse clicking for shop buttons
+        buttonHandlerOptions(buttons.finished); //Handles button logic for "finished" button (exception)
     } else if (state === "finished") {
-        finishedController();
+        //Hi, the mouse click events for the finished state is in the buttonHandler function, don't ask me why
     }
 }
 
+/**
+ * Resets all the game properties
+ */
 function resetGame() {
     backgroundImage = "";
     colorSelect = "";
@@ -80,17 +82,20 @@ function resetGame() {
     }
 }
 
-function finishedController() {
-}
-
 //IDK wtf is going on over here
+/**
+ * Handles the button logic for the game
+ * @param button The properties of the current button
+ */
 function buttonHandler(button) {
+    //Calculate the min and max bounds
     const minX = button.x - button.w / 2;
     const maxX = button.x + button.w / 2;
     const minY = button.y - button.h / 2;
     const maxY = button.y + button.h / 2;
 
-    //Button handling (play)
+    //Change states accorsding to which button was pressed
+    //Yes, I got rid of 3. It's not a mistake
     if (mouseX > minX && mouseX < maxX && mouseY > minY && mouseY < maxY && clicked === true) {
         if (button.key === 0) {
             state = "choose";
@@ -99,9 +104,6 @@ function buttonHandler(button) {
             resetGame();
         } else if (button.key === 2) { //Does not get triggered for some reason
             state = "decorate";
-        } else if (button.key === 3) {
-            state = "title";
-            resetGame();
         } else if (button.key === 4) { //Does not get triggered for some reason
             state = "finished";
         } else if (button.key === 5) {
@@ -112,12 +114,15 @@ function buttonHandler(button) {
         } else if (button.key === 7) {
             state = "decorate";
             resetCounter();
-            console.log(totalFlies);
         }
     }
     clicked = false;
 }
 
+/**
+ * Main mousePressed function that calls the other mousePressed functions depending on the state
+ *
+ */
 function mousePressed() {
     if (state === "frog") {
         mousePressedFrog();
@@ -126,10 +131,13 @@ function mousePressed() {
     }
 }
 
+/**
+ * Main mouseReleased function that calls other mouseReleased functions
+ */
 function mouseReleased() {
+    //What a lonely function, all by their lonesome, being lonely
     mouseReleasedDecorate();
 }
-
 
 /**
  * If the user clicks 's' and is in the end state, they will download a PNG of their creation
@@ -143,12 +151,3 @@ function keyTyped(event) {
     }
 
 }
-
-//Bugs
-//my buttons in general are wank
-
-//Optional todo
-//Price index in frogfrogfrog
-//Fly swarm in frogfrogfrog
-//Make tongue catching more intuitive
-
