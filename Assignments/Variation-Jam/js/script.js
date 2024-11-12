2a84aaf771c36b12d7814281cce051b024cce8e3
Diana <edvi.diana@gmail.com>
2024-11-12 16:27:05 -0500

    Added bouncing ball

--- Assignments/Variation-Jam/js/script.js
+++ Assignments/Variation-Jam/js/script.js
@@ -12,27 +12,93 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
  */
 
-let b1;
+
+let buttons = {
+    soloButton: undefined,
+    mathButton: undefined,
+    colorButton: undefined,
+    questionButton: undefined
+}
+let states = {
+    current: undefined,
+    menu: "menu",
+    soloPong: "solo",
+    mathPong: "math",
+    colorPong: "color",
+}
 
 function setup() {
     canvas = createCanvas(640, 640);
-    background(0);
+    background("#6160b2");
+    states.current = states.menu;
+    // menu();
 
-    b1 = new Button("button", 100, 100, 150, 80, boo);
-    b1.buttonStyles("blue");
-
-    let b2 = new Button("ahh", 400, 400, 300, 200, () => {
-        console.log("ahhhhh");
-    });
-    b2.buttonStyles("red", "90px");
-}
-
-function boo() {
-    console.log("Hello world");
 }
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
  */
 function draw() {
+    background("#6160b2");
+    soloPong(paddle, ball);
 }
+
+let paddle = {x: 320, y: 609, w: 100, h: 30, f: "white", speed: 10};
+let ball = {x: 320, y: 320, w: 50, f: "white", speedY: 5, speedX: 3}; // Added speedX for horizontal movement
+
+function soloPong(paddle, ball) {
+    // Draw paddle
+    push();
+    fill(paddle.f);
+    rect(paddle.x, paddle.y, paddle.w, paddle.h);
+    pop();
+
+    // Draw ball
+    push();
+    fill(ball.f);
+    ellipse(ball.x, ball.y, ball.w);
+    pop();
+
+    // Move ball
+    ball.x += ball.speedX;
+    ball.y += ball.speedY;
+
+    // Move paddle based on key presses
+    if (keyIsDown(LEFT_ARROW)) {
+        paddle.x -= paddle.speed;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+        paddle.x += paddle.speed;
+    }
+
+    // Constrain paddle within canvas boundaries
+    paddle.x = constrain(paddle.x, 0, width - paddle.w);
+
+    // Check collisions with top wall
+    if (ball.y - ball.w / 2 <= 0) {
+        ball.speedY *= -1;
+    }
+
+    // Check collisions with side walls
+    if (ball.x - ball.w / 2 <= 0 || ball.x + ball.w / 2 >= width) {
+        ball.speedX *= -1;
+    }
+
+    // Check collision with paddle
+    if (
+        ball.y + ball.w / 2 >= paddle.y && ball.x > paddle.x && ball.x < paddle.x + paddle.w) {
+        // Reverse vertical direction
+        ball.speedY *= -1;
+
+        // Change horizontal direction based on where it hits the paddle
+        let hitPoint = (ball.x - paddle.x) / paddle.w; // Range: 0 (left) to 1 (right)
+        if (hitPoint < 0.5) {
+            ball.speedX = -abs(ball.speedX); // Go left
+        } else {
+            ball.speedX = abs(ball.speedX); // Go right
+        }
+    }
+}
+
+
+
